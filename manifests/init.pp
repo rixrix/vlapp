e6ac43364a5aa4a16690525914d3f18469838c51
Richard Sentino <richard@mingdinative.com>
2013-10-07 00:14:25 +1300

    removed modules and moved to shell provisioning. added license, and other minor misc. changes

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,57 +1,63 @@
+/*
+
+Copyright (c) 2013, Richard Sentino
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 node default {
-  include init
-  include packages
+
+  stage { 'pre':
+    before => Stage['main']
+  }
+
+  stage { 'package':
+    require => Stage['pre']
+  }
+
+  class { 'init':
+    stage => pre
+  }
+
+  class { 'packages':
+    stage => package
+  }
 }
 
 /***********************************************************************************************************************
  * Packages
  ***********************************************************************************************************************/
 class packages {
-  include packages::install
-}
-
-class packages::install {
-  if defined(Class['puppet::modules']) {
-    #include postgresql::server
-  }
-}
-
-/***********************************************************************************************************************
- * Modules (package dependencies)
- ***********************************************************************************************************************/
-class puppet::modules {
-  exec { 'PostgreSQL':
-    command => 'puppet module install --module_repository=http://forge.puppetlabs.com --force puppetlabs-postgresql',
-    path => ['/usr/bin', '/bin']
-  }
-
-  exec { 'Standard Library':
-    command => 'puppet module install --module_repository=http://forge.puppetlabs.com --force puppetlabs-stdlib',
-    path => ['/usr/bin', '/bin']
-  }
-
-  exec { 'Firewall':
-    command => 'puppet module install --module_repository=http://forge.puppetlabs.com --force puppetlabs-firewall',
-    path => ['/usr/bin', '/bin']
-  }
-
-  exec { 'APT':
-    command => 'puppet module install --module_repository=http://forge.puppetlabs.com --force puppetlabs-apt',
-    path => ['/usr/bin', '/bin']
-  }
-
-  exec { 'Concat':
-    command => 'puppet module install --module_repository=http://forge.puppetlabs.com --force ripienaar-concat',
-    path => ['/usr/bin', '/bin']
-  }
+    class {'postgresql::server':
+    }
 }
 
 /***********************************************************************************************************************
  * Initialization
+ *  - repos, etc
  ***********************************************************************************************************************/
 class init {
-  include init::repo
-  include puppet::modules
+  class { 'init::repo':}
 }
 
 class init::repo {
